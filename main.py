38624adbac3a436eb77f51355e3fbc56bbe35d72
Elton Aquino <87995437+elton-aq@users.noreply.github.com>
2024-09-10 18:40:56 -0300

    front capturando audio

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
 import gradio as gr
 import src.chat as chat
 import src.talking as talking
-import speech_recognition as sr
+import src.speech as speech
 
-# Função para capturar áudio e transcrevê-lo usando o speech_recognition
-def capturaAudio(audio):
-    recognizer = sr.Recognizer()
-    print(f'Áudio recebido: {audio}')
-
-    if audio is None:
-        return 
-
-    # Abre o arquivo de áudio recebido e ajusta para ruído ambiente
-    with sr.AudioFile(audio) as source:
-        print(f'Áudio aberto: {source}')
-        recognizer.adjust_for_ambient_noise(source, duration=1)
-        audio_data = recognizer.record(source)
-        print(f'Áudio gravado: {audio_data}')
-
-    try:
-        # Transcrição usando a API do Google para português
-        text = recognizer.recognize_google(audio_data, language='pt-BR')
-        return text, True
-    except sr.UnknownValueError:
-        return "Não foi possível reconhecer o áudio.", False
-    except sr.RequestError as e:
-        return f"Erro ao solicitar resultados da API: {e}", False
-    
 # Função para processar o áudio e gerar a resposta
 def process_audio(audio, chat_history):
     try:
-        # Exibe a mensagem de processamento e transcreve o áudio
-        yield [(f"Processando fala...", None)], None, "Aguarde um segundo e fale algo.", chat_history
-        question, ret = capturaAudio(audio)
+        question, ret = speech.capturaAudio(audio)
         if not ret:
             raise Exception(question)
 
@@ -57,8 +31,8 @@ def process_audio(audio, chat_history):
             for msg in chat_history
         ]
 
-        # Exibe a resposta no chat e o áudio gerado
-        yield chat_display, audio_file_path, "Processamento completo.", chat_history
+        # Após o processamento, resetar o campo de áudio do usuário (sem afetar o áudio da resposta)
+        yield chat_display, audio_file_path, "Clique em RECORD para começar a falar", chat_history
 
     except Exception as e:
         # Em caso de erro, exibe uma mensagem
@@ -81,7 +55,7 @@ with gr.Blocks() as app:
 
     status_display = gr.Textbox(
         label="Status",
-        value="Por favor, clique no botão para começar a falar",
+        value="Clique em RECORD para começar a falar",
         interactive=False,
         lines=1
     )
@@ -92,11 +66,19 @@ with gr.Blocks() as app:
     # Definição do botão de entrada do áudio e chamada para processar o áudio
     audio_input = gr.Audio(sources=["microphone"], type="filepath")
 
-    # Conectando a função process_audio com a interface Gradio
-    audio_input.change(
+    submit_btn = gr.Button("Enviar")
+
+    submit_btn.click(
         fn=process_audio,
         inputs=[audio_input, chat_history_state],
-        outputs=[chatbot, gr.Audio(type="filepath", autoplay=True, visible=False), status_display, chat_history_state],
+        outputs=[chatbot, gr.Audio(type="filepath", autoplay=True, visible=False), status_display, chat_history_state]
+    )
+
+    # Reseta o campo de áudio gravado pelo usuário após o envio
+    submit_btn.click(
+        fn=lambda: None,
+        inputs=[],
+        outputs=[audio_input]
     )
 
 app.launch()
